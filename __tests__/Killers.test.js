987e3b997cab17656193620c4e43d8d98834a466
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-04 16:36:55 -0600

    get all killers pass

--- __tests__/Killers.test.js
+++ __tests__/Killers.test.js
@@ -10,32 +10,20 @@ describe('backend-express-template routes', () => {
   it('#GET /killers should return a list of killers', async () => {
     const res = await request(app).get('/killers');
     expect(res.status).toEqual(200);
-    expect(res.body).toEqual([
-      (
-        'John Wayne Gacy',
-        '33',
-        '1942',
-        '1968'
-      ),
-      (
-        'Jeffrey Dahmer',
-        '17',
-        '1906',
-        '1992'
-      ),
-      (
-        'Ted Bundy',
-        '25',
-        '1946',
-        '1979'
-      ),
-      (
-        'H H Holmes',
-        '27',
-        '1861',
-        '1895'
+    expect(res.body).toEqual(expect.arrayContaining([
+      expect.objectContaining(
+
+        {
+          id: expect.any(String),
+          name: expect.any(String),
+          victims: expect.any(Number),
+          born: expect.any(Number),
+          convicted:expect.any(Number)
+        }
       )
-    ]);
+
+    ])
+    );
   });
   afterAll(() => {
     pool.end();
